a3ed762dd468552f57f8e47f0047a78d1dff6d11
Daniel Kessl <dan@kessl.net>
2024-04-30 22:55:00 +0300

    Broadcast unsubscribe

--- app/channels/session_channel.rb
+++ app/channels/session_channel.rb
@@ -1,5 +1,10 @@
 class SessionChannel < ApplicationCable::Channel
+  after_unsubscribe do
+    ActionCable.server.broadcast('sessions', { type: 'unsubscribe', client_id: @client_id })
+  end
+
   def subscribed
+    @client_id = params[:client_id]
     stream_from 'sessions'
   end
 
